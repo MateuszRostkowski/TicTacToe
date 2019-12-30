424f00eee65a876c0e5a9eb5d497129ead3ec4ca
Mateusz.rostkowsky995@gmail.com <mateusz.rostkowsky995@gmail.com>
2019-12-30 14:13:37 +0100

    Updates

---
+++ precache-manifest.eaaaffaa9165ad608f55d66a37c2d2a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "139911eca8157970871ff281e64696c1",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "d7dc67d114e43ce863be",
+    "url": "/tictactoe/static/css/main.2b5ad575.chunk.css"
+  },
+  {
+    "revision": "93c4319319f992928171",
+    "url": "/tictactoe/static/js/2.244b2b74.chunk.js"
+  },
+  {
+    "revision": "d7dc67d114e43ce863be",
+    "url": "/tictactoe/static/js/main.bf9d088f.chunk.js"
+  },
+  {
+    "revision": "e9d42febdd2440e2e0cd",
+    "url": "/tictactoe/static/js/runtime~main.1cfd47c4.js"
+  }
+]);
